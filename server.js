ca8d394f650b822e12b35f7de5cca762385d124f
IsaacH <Isaac.hathaway28@gmail.com>
2022-02-27 08:02:43 -0700

    connect database and make models directory and file

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url'
 import logger from 'morgan'
 
 //to connect database with mongoose
-import ('./config/database.js')
+import('./config/database.js')
 
 // import routers
 import { router as indexRouter } from './routes/index.js'
