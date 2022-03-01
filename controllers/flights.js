7a2c9d15b9946823b34a249cbfae3605c76ed2fa
IsaacH <Isaac.hathaway28@gmail.com>
2022-03-01 16:41:44 -0700

    make meals schema and add new crud operation to add meals

--- controllers/flights.js
+++ controllers/flights.js
@@ -11,7 +11,6 @@ function newFlight (req, res) {
 
 function create(req, res) {
   const flight = new Flight(req.body)
-  console.log('sanity')
   flight.save(function(err) {
     if (err) return res.redirect('/flights/new')
     console.log('sanity 2');
---
+++ controllers/meals.js
@@ -0,0 +1,9 @@
+import { Meal } from '../models/meal.js'
+
+function newMeal(req, res) {
+  console.log('sanity');
+}
+
+export {
+  newMeal as new
+}
